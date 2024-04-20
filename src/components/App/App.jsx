63704ba7ce91720cb05e4b2624bb9b38e03d8e09
Jose Lopez Renteria <joselukas24@gmail.com>
2024-04-19 21:51:35 -0500

    initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+function App() {
+  return <Main>This is the Main component</Main>;
+}
+
+const Main = styled.div`
+  border: 2px solid red;
+  height: 100%;
+`;
+
+export default App;
