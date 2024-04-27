e18abd984bed730820178465592c74c39259d698
Jose Lopez Renteria <joselukas24@gmail.com>
2024-04-26 22:36:59 -0500

    done with the checkout page except the card form

---
+++ src/components/Checkout/Checkout.jsx
@@ -0,0 +1,107 @@
+import styled from "styled-components";
+import { cart_data } from "../../data";
+
+function Checkout() {
+  return (
+    <Wrapper>
+      <OrderSummary>
+        Order Summary
+        <p style={{ color: "rgb(156, 163, 175)", fontSize: "1rem" }}>
+          Check your items. And Select a suitable shipping method.
+        </p>
+        <CartItems>
+          {cart_data.map((game) => {
+            return (
+              <CartItem key={game.game_id}>
+                <Cover src={game.sample_cover_image} alt="" />
+                <GameInfo>
+                  <GameTitle>{game.title}</GameTitle>
+                  <GamePrice>${game.price}</GamePrice>
+                </GameInfo>
+              </CartItem>
+            );
+          })}
+        </CartItems>
+      </OrderSummary>
+      <PaymentDetails>
+        Payment Details
+        <p style={{ color: "rgb(156, 163, 175)", fontSize: "1rem" }}>
+          Complete your order by providing your payment details.{" "}
+        </p>
+        Finish at a later date.
+      </PaymentDetails>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  //   border: 2px solid red;
+  display: flex;
+  gap: 20px;
+  padding-top: 50px;
+
+  @media (max-width: 1100px) {
+    flex-direction: column;
+  }
+`;
+
+const OrderSummary = styled.div`
+  //   border: 2px solid springgreen;
+  width: 50%;
+  padding-top: 40px;
+  padding-left: 20px;
+  padding-right: 20px;
+
+  @media (max-width: 1100px) {
+    width: 100%;
+    padding: 10px;
+  }
+`;
+
+const PaymentDetails = styled.div`
+  //   border: 2px solid springgreen;
+  background-color: rgb(249, 250, 251);
+  width: 50%;
+  padding-top: 40px;
+  padding-left: 16px;
+  padding-right: 16px;
+  border-radius: 5px;
+
+  @media (max-width: 1100px) {
+    width: 100%;
+  }
+`;
+
+const CartItems = styled.div`
+  border: 1px solid rgb(229, 231, 235);
+  border-radius: 8px;
+  width: 100%;
+  margin-top: 32px;
+  padding: 16px 24px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const CartItem = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+const Cover = styled.img`
+  width: 100px;
+  border-radius: 6px;
+`;
+
+const GameInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-top: 10px;
+  font-size: 1.1rem;
+`;
+
+const GameTitle = styled.p``;
+
+const GamePrice = styled.p``;
+
+export default Checkout;
