48c16b98268c16633ae4558da50de15fe07eeb22
Jose Lopez Renteria <joselukas24@gmail.com>
2024-07-10 12:12:16 -0500

    fixed the weird sizing in the game cards when only 2 rows of games are present

--- src/components/MarketPlace/GameGrid.jsx
+++ src/components/MarketPlace/GameGrid.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { QUERIES } from "../../constants";
 
 export default function GameGrid({ gameList, gamesPerPage, currentPage }) {
   return (
@@ -9,7 +10,12 @@ export default function GameGrid({ gameList, gamesPerPage, currentPage }) {
         .map((game) => (
           <GameCard to={`/product/${game.game_id}`} key={game.game_id}>
             <GameCoverWrapper>
-              <GameCover src={game.sample_cover_image} alt="" />
+              <GameCover>
+                <GameCoverImage
+                  src={game.sample_cover_image}
+                  alt={`Game Cover for the game ${game.title}`}
+                />
+              </GameCover>
             </GameCoverWrapper>
 
             <GameInfo>
@@ -26,9 +32,13 @@ const Grid = styled.div`
   // border: 3px dotted springgreen;
   flex: 5;
   padding: 30px;
-  gap: 75px;
+  gap: 50px;
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  grid-template-rows: repeat(3, 1fr);
+  @media (${QUERIES.laptopAndSmaller}) {
+    gap: 40px;
+  }
 
   @media (max-width: 350px) {
     grid-template-columns: 1fr;
@@ -36,7 +46,7 @@ const Grid = styled.div`
 `;
 
 const GameCard = styled(Link)`
-  // border: 3px solid red;
+  border: 1px solid black;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -47,18 +57,29 @@ const GameCard = styled(Link)`
 `;
 
 const GameCoverWrapper = styled.div`
-  // border: 3px dashed black;
+  // border: 3px dashed white;
   flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   background-color: black;
-  display: grid;
-  place-content: center;
+  width: 100%;
 `;
 
-const GameCover = styled.img`
+const GameCover = styled.div`
+  // border: 3px dashed green;
+  overflow: hidden;
+  width: 100%;
+`;
+
+const GameCoverImage = styled.img`
   // border: 3px solid blue;
-  min-width: 0;
+  max-width: 100%;
+  overflow: hidden;
+  object-fit: contain;
 `;
 
 const GameInfo = styled.div`
   // border: 3px solid springgreen;
+  text-align: center;
 `;
