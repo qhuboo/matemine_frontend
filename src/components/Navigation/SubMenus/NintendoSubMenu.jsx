8b21e378024a89978193b06addb91a769fff7a7b
Jose Lopez Renteria <joselukas24@gmail.com>
2024-07-27 23:49:33 -0500

    start work on submenu contents

--- src/components/Navigation/SubMenus/NintendoSubMenu.jsx
+++ src/components/Navigation/SubMenus/NintendoSubMenu.jsx
@@ -1,12 +1,31 @@
 import SubMenuWrapper from "./SubMenuWrapper";
 import platforms from "../../../platform_data";
+import styled from "styled-components";
 
 export default function NintendoSubMenu({ $isActive }) {
   return (
     <SubMenuWrapper $isActive={$isActive}>
-      {platforms.nintendo.map((console) => (
-        <div key={console}>{console}</div>
-      ))}
+      <SubMenuContentWrapper>
+        <LinksWrapper></LinksWrapper>
+        <FeaturedWrapper></FeaturedWrapper>
+      </SubMenuContentWrapper>
     </SubMenuWrapper>
   );
 }
+
+const SubMenuContentWrapper = styled.div`
+  border: 3px solid red;
+  display: flex;
+  gap: 150px;
+  height: 100%;
+`;
+
+const LinksWrapper = styled.div`
+  border: 2px solid green;
+  flex: 1.25;
+`;
+
+const FeaturedWrapper = styled.div`
+  border: 2px solid springgreen;
+  flex: 1;
+`;
