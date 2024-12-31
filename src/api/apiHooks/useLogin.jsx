14dd27a54b261af240fc336cb481aecc9115cf3a
Jose Lopez Renteria <joselukas24@gmail.com>
2024-12-30 21:52:01 -0600

    removed cart prefetch, will create custom hook later

--- src/api/apiHooks/useLogin.jsx
+++ src/api/apiHooks/useLogin.jsx
@@ -1,10 +1,9 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import api from "../api";
 import useAuth from "../../components/Auth/hooks/useAuth";
 import { useNavigate } from "react-router-dom";
 
 export default function useLogin(destination) {
-  const queryClient = useQueryClient();
   const user = useAuth();
   const navigate = useNavigate();
 
@@ -13,16 +12,6 @@ export default function useLogin(destination) {
     onSuccess: async (data) => {
       if (data.isAuthenticated) {
         user.login(data);
-        await queryClient.prefetchQuery({
-          queryKey: ["cart"],
-          queryFn: api.protectedGet(
-            `${import.meta.env.VITE_BACKEND_URL}/cart/`,
-            {
-              accessToken: data?.accessToken,
-              email: data?.email,
-            }
-          ),
-        });
         navigate(destination);
       }
     },
