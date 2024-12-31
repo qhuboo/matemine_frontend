c25273c28acb9779336b422bcc33dad04547a277
Jose Lopez Renteria <joselukas24@gmail.com>
2024-12-30 21:50:18 -0600

    custom hook to retrieve the payment intent

---
+++ src/api/apiHooks/useGetPaymentIntent.jsx
@@ -0,0 +1,45 @@
+import { useQuery } from "@tanstack/react-query";
+import api from "../api";
+import useAuth from "../../components/Auth/hooks/useAuth";
+import { useEffect } from "react";
+
+export default function useGetPaymentIntent() {
+  const user = useAuth();
+
+  const paymentIntent = useQuery({
+    queryKey: ["paymentIntent"],
+    queryFn: api.protectedGet(
+      `${import.meta.env.VITE_BACKEND_URL}/stripe/get-payment-intent`,
+      {
+        accessToken: user?.accessToken,
+        email: user?.user?.email,
+      }
+    ),
+    enabled: user?.isAuthenticated,
+    retry: (failureCount, error) => {
+      console.log(error.message);
+      if (error.message === "Refresh token is expired") {
+        return false;
+      }
+      return true;
+    },
+    gcTime: Infinity,
+  });
+
+  useEffect(() => {
+    if (
+      paymentIntent?.status === "success" &&
+      paymentIntent?.data?.accessToken
+    ) {
+      if (user?.accessToken !== paymentIntent?.data?.accessToken) {
+        user?.updateAccessToken(paymentIntent?.data?.accessToken);
+      }
+    } else if (paymentIntent?.isError) {
+      if (paymentIntent?.error?.message === "Refresh token is expired") {
+        user?.sessionExpired();
+      }
+    }
+  }, [paymentIntent, user]);
+
+  return paymentIntent;
+}
